d8b7d170a76e524f26077c18edf0251903bdce58
mahmoud helal <mhelal@adam.ai>
2021-08-21 00:30:31 +0200

    fix read more route issue

--- src/components/pages/news/ReadMoreBtn.js
+++ src/components/pages/news/ReadMoreBtn.js
@@ -1,13 +1,11 @@
 import React from "react";
-import { BrowserRouter, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function ReadMoreBtn() {
   return (
-    <BrowserRouter forceRefresh={true}>
       <Link to="/readmore" className="read-btn p-2">
         Read More →
       </Link>
-    </BrowserRouter>
   );
 }
 
